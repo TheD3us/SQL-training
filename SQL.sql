530075ea60654fb2286fb76a68a7abab16c4f528
unknown <theo.mezier@gmail.com>
2022-04-06 17:01:47 +0200

    Ajout travaux SQL

--- SQL.sql
+++ SQL.sql
@@ -1,4 +1,7 @@
 DROP TABLE conges, conges_mens, employes, services;
+
+
+
 CREATE TABLE employes
 (
 	code_emp INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -8,7 +11,9 @@ CREATE TABLE employes
     date_embauche DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     salaire DECIMAL(8,2) NOT NULL DEFAULT 0,
     code_service CHAR(5) NOT NULL,
-    code_chef INT
+    code_chef INT,
+    FOREIGN KEY (code_service) REFERENCES services(code_service),
+    FOREIGN KEY (code_emp) REFERENCES employes(code_emp)
     
 );
 
@@ -22,13 +27,17 @@ CREATE TABLE conges
 (
 	code_emp INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     annee NUMERIC(4,0) NOT NULL,
-    nb_jours_acquis NUMERIC(2,0) DEFAULT 30
+    nb_jours_acquis NUMERIC(2,0) DEFAULT 30,
+    FOREIGN KEY (code_emp) REFERENCES employes(code_emp)
+    
 );
 
 CREATE TABLE conges_mens
 (
-	code_emp INT PRIMARY KEY AUTO_INCREMENT,
-    annee NUMERIC(4,0) PRIMARY KEY,
-    mois NUMERIC(2,0) PRIMARY KEY,
-    nb_jours_pris NUMERIC(2,0) DEFAULT 0
+	code_emp INT AUTO_INCREMENT,
+    annee NUMERIC(4,0),
+    mois NUMERIC(2,0),
+    nb_jours_pris NUMERIC(2,0) DEFAULT 0,
+    CONSTRAINT PK_conges_mens PRIMARY KEY (code_emp, annee, mois),
+    FOREIGN KEY (code_emp, annee) REFERENCES conges(code_emp, annee)
 );
